fc67d5d9d22717f24c6d0f1c6d1337b50c9b66e2
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-31 22:19:44 +0000

    adding changes for task 6

---
+++ models/engine/db_storage.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""This is the DBStorage class for AirBnB"""
+
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class DBStorage():
+    """class to manage MySql DB storage"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """engine init"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv("HBNB_MYSQL_USER"),
+            getenv("HBNB_MYSQL_PWD"),
+            getenv("HBNB_MYSQL_HOST"),
+            getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
+
+        if getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """ show all results """
+        result = []
+        if cls:
+            result = self.__session.query(cls).all()
+        else:
+            cl_list = ["State", "City", "User", "Place", "Review", "Amenity"]
+            for class_name in cl_list:
+                temp = self.__session.query(eval(cl_list)).all()
+                result.extend(temp)
+
+        result_dict = {}
+        for res in result:
+            key = "{}.{}".format(type(key).__name__, key.id)
+            value = key
+            result_dict[key] = value
+        return result_dict
+
+    def new(self, obj):
+        """ add new object to session """
+        if obj:
+            self.__session.add(obj)
+
+    def save(self):
+        """ save changes """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete from session """
+        if obj:
+            self.__session.query(obj).delete(synchronize_session='fetch')
+
+    def reload(self):
+        """create all tables in the database (feature of SQLAlchemy)"""
+        Base.metadata.create_all(self.__engine)
+        session_fact = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_fact)
+        self.__session = Session
