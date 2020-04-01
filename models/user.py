297fa208cda07a8bc6bcb7d74b96d5de8b4a4c7b
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-04-01 23:33:48 +0000

    fixes in user

--- models/user.py
+++ models/user.py
@@ -21,7 +21,5 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    places = relationship('Place', cascade='all, delete-orphan',
-                            backref='user')
-    reviews = relationship('Review', cascade='all, delete-orphan',
-                            backref='user')
+    """ places = relationship('Place', backref='user', cascade='all, delete')
+    reviews = relationship('Review', backref='user', cascade='all, delete') """
