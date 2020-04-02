80f9c9e68674f7cd07879a55c7ecb3b4d8cad138
steven-cruz <stevencruz.300008@gmail.com>
2020-04-02 00:16:55 +0000

    tasks 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
@@ -8,4 +10,6 @@ class Amenity(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_anenity")
