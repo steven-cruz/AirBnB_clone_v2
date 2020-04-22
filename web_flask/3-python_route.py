a59588556b31d80d869d473a3537af9605bcd573
steven-cruz <stevencruz.300008@gmail.com>
2020-04-22 05:04:41 +0000

    default value in the variable <text>

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,28 +2,29 @@
 # starts a flask application on 0.0.0.0 port 5000
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """ Python function that prints Hello HBNB! """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """ Python function that prints HBNB """
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_text(text):
     """ Python function that prints C follewed by value of the <text> """
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python')
+@app.route('/python/<text>')
 def python_text(text="is cool"):
     """
     Python function that prints Python follewed by value of the <text>
