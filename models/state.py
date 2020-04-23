78e810ae7112a76d385d6828e0fe83de1b1113fb
steven-cruz <stevencruz.300008@gmail.com>
2020-04-23 03:30:54 +0000

    upgrade

--- models/state.py
+++ models/state.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-"""This is the state class"""
+'''
+    Implementation of the State class
+'''
+
 from models.base_model import BaseModel, Base
 import sqlalchemy
 import models
-from models.city import City
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from os import getenv
 from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
-    name = Column(
-        String(128),
-        nullable=False
-    )
-    cities = relationship(
-        "City",
-        backref="state",
-        cascade="all, delete"
-    )
+    '''
+        Implementation for the State.
+    '''
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
         @property
-        def City(self):
-            """ get the cities """
-            cities_dic = []
+        def cities(self):
+            """Setter Method to list cities"""
+            city_list = []
             for key, value in models.storage.all().items():
                 try:
                     if value.state_id == self.id:
-                        cities_dic.append(value)
+                        city_list.append(value)
                 except BaseException:
                     pass
-            return cities_dic
+            return city_list
+    else:
+        cities = relationship('City', cascade='all, delete', backref='state')
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
