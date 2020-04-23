492a43195ac30cd1e7df7fef8f19cee87aca0cea
steven-cruz <stevencruz.300008@gmail.com>
2020-04-23 03:53:27 +0000

    upgrade

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,13 @@
 -- Create Database and grant permission to User
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT USAGE ON * . *
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
